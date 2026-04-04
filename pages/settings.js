24aa60c7507dddd681abc399bac488abb1cdc7f1
kancharajesh <rajeshkancha41@gmail.com>
2026-04-04 15:01:13 +0530

    settings

--- pages/settings.js
+++ pages/settings.js
@@ -1,20 +1,95 @@
-import { expect } from "@playwright/test";
+import { test, expect } from "@playwright/test";
+import { Login_model } from "../pages/Login_model.js";
+import { Settings } from "../pages/settings.js";
 
-export class settings {
-    constructor(page){
-        this.page = page;
- 
+test.beforeEach(async ({ page }) => {
+  const login = new Login_model(page);
 
-    this.setting_profile = page.locator("body > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(3) > div:nth-child(2)");
-    this.profile_popup = page.locator("//div[@class='pt-4 w-full']");
+  await login.launchTheBrowser();
+  await login.openLoginForm();
+  await login.login("rajesh@jupitermeta.io", "12345678");
 
-    // Free credits.
-    this.Get_free_credits = page.locator("//div[@class='pt-4 w-full']");
-    this.Get_free_credits_popup = page.locator("//div[@role='dialog']");
-    this.Get_free_credits_popup_close = page.locator("//img[@alt='close-modal']");
-    
-    //
+  await page.waitForLoadState("domcontentloaded");
+});
 
+test("Verify profile menu opens", async ({ page }) => {
+  const settings = new Settings(page);
 
-}
-}
+  await settings.openProfileMenu();
+  await settings.verifyProfilePopup();
+});
+
+test("Verify profile credits are visible", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openProfileMenu();
+  await settings.verifyProfileCredits();
+});
+
+test("Verify Get Free Credits popup opens", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openProfileMenu();
+  await settings.openFreeCredits();
+});
+
+test("Verify Get Free Credits popup closes", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openProfileMenu();
+  await settings.openFreeCredits();
+  await settings.closeFreeCreditsPopup();
+});
+
+test("Verify General settings page", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openGeneralSettings();
+  await settings.verifyGeneralSettings();
+});
+  
+//rajesh
+test("Verify Account settings page", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openAccountSettings();
+  await settings.verifyAccountSettings();
+});
+
+test("Verify Billing settings page", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openBillingSettings();
+  await settings.verifyBillingSettings();
+});
+
+test("Verify Help page", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openHelp();
+  await settings.verifyHelpPage();
+});
+
+test("Verify Dashboard page", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openDashboard();
+  await settings.verifyDashboardPage();
+});
+
+test("Verify Logout popup opens", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openProfileMenu();
+  await settings.clickLogout();
+  await settings.verifyLogoutPopup();
+});
+
+test("Verify Logout popup cancel button", async ({ page }) => {
+  const settings = new Settings(page);
+
+  await settings.openProfileMenu();
+  await settings.clickLogout();
+  await settings.cancelLogout();
+  await expect(settings.logout_popup).toBeHidden();
+});
