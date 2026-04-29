000ed581c23d298ade405492db67508024effb91
Rajesh Kancha <rajeshkancha41@gmail.com>
2026-04-29 13:14:17 +0530

    yml update

--- playwright.config.js
+++ playwright.config.js
@@ -2,36 +2,43 @@ const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "./tests",
-  reporter: "html",
-  timeout: 30000,
-  workers: 1, // 👈 runs tests sequentially
+
+  reporter: [["html", { open: "never" }], ["list"]],
+
+  timeout: 60000,
+
+  workers: process.env.CI ? 1 : 1,
+
+  retries: process.env.CI ? 1 : 0,
 
   expect: {
-    timeout: 15000,
+    timeout: 20000,
   },
 
   use: {
-    trace: "on-first-retry",
+    trace: "retain-on-failure",
     screenshot: "only-on-failure",
     video: "retain-on-failure",
-    // headless: false
+    headless: true,
+    actionTimeout: 30000,
+    navigationTimeout: 60000,
   },
-  
 
   projects: [
-  {
-    name: "chromium",
-    use: { ...devices["Desktop Chrome"] },
-    testIgnore: ["**/mobile_*.spec.js"],
-  },
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+      testIgnore: ["**/mobile_*.spec.js"],
+    },
+
     {
       name: "Mobile Chrome",
       use: {
         ...devices["Pixel 5"],
       },
-      testMatch: [
-        "**/mobile_*.spec.js",
-      ],
+      testMatch: ["**/mobile_*.spec.js"],
     },
   ],
 });
